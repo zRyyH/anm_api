965a386733b8596be5b305a2761afb9184a1771b
zRyyH <zryyh.br@gmail.com>
2025-07-18 20:49:04 -0300

    att 1.2

--- main.py
+++ main.py
@@ -89,4 +89,4 @@ def send_email(request_token: str, email: str, _=Depends(check_auth)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
